6f8fcc6b90f93331eabcd98d7c10d3a57081350f
RashedRifat88 <rasrif88@gmail.com>
2024-09-12 20:13:08 +0600

    second ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">Hello riaft world!</h1>
+      <Header />
     </>
   );
 }
