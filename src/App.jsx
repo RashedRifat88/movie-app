954bd6595ff9c2210b1c3d15e8ba72ecf8d605c3
RashedRifat88 <rasrif88@gmail.com>
2024-09-14 19:10:00 +0600

    Movie details modal...

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import MovieList from "./cine/MovieList";
+import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
@@ -12,10 +14,13 @@ function App() {
       <Header />
 
       <main>
-        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
+          <MovieList/>
         </div>
       </main >
+
+      <Footer />
 
     </>
   );
