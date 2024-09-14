b619aa7557ddf9bb06dd2e4bd252d2ca33507e30
RashedRifat88 <rasrif88@gmail.com>
2024-09-14 11:04:55 +0600

    third...

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main >
+
     </>
   );
 }
