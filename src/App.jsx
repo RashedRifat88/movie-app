1f9a15ad113adb07dfda2235d2e342f220d327f1
RashedRifat88 <rasrif88@gmail.com>
2024-09-15 18:54:04 +0600

    Add to cart....

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,29 @@ import MovieList from "./cine/MovieList";
 import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { MovieContext } from "./context";
+import { useState } from "react";
 
 function App() {
 
+    const [cartData, setCartData] = useState([]);
+
   return (
     <>
-      <Header />
+      <MovieContext.Provider value={{cartData, setCartData}}>
 
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList/>
-        </div>
-      </main >
+        <Header />
 
-      <Footer />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
 
+        <Footer />
+
+      </MovieContext.Provider>
     </>
   );
 }
--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { getImageUrl } from "../utils/cine-utility";
 import Rating from "./Rating";
 import MovieDetailsModal from "./MovieDetailsModal";
+import { MovieContext } from "../context";
+
+
 
 export default function MovieCard({ movie }) {
   const [showModal, setShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const {cartData, setCartData} = useContext(MovieContext);
 
   function handleModalClose() {
     setSelectedMovie(null);
@@ -17,14 +22,33 @@ export default function MovieCard({ movie }) {
     setShowModal(true);
   }
 
+  function handleAddToCart(event, movie) {
+    event.stopPropagation();
+    console.log(movie);
+    
+
+    const found = cartData.find((item) => {
+        return item.id === movie.id;
+    })
+
+    console.log(found);
+
+    if(!found){
+            setCartData([...cartData, movie]);
+    }else{
+            console.error(`The movie ${movie.title} is already been added to the cart!`);
+    }
+    
+  }
+
   return (
     <>
       {showModal && (
-        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} onCartAdd={handleAddToCart} />
       )}
 
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-        <a href="#" onClick={ ()=> handleMovieSelection(movie)}>
+        <a href="#" onClick={() => handleMovieSelection(movie)}>
           <img
             className="w-full object-cover"
             src={getImageUrl(movie.cover)}
@@ -44,6 +68,7 @@ export default function MovieCard({ movie }) {
             <a
               className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
               href="#"
+              onClick={(e) => handleAddToCart(e, movie)}
             >
               <img src="./assets/tag.svg" alt="" />
               <span>${movie.price} | Add to Cart</span>
